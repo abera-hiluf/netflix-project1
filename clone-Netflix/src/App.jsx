e157ec24e33c5a1ffdd240e2f0b3bcdc5b960fef
abz <teshalea000@gmail.com>
2025-05-11 20:17:08 +0300

    header and footer

--- clone-Netflix/src/App.jsx
+++ clone-Netflix/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
 
 
   return (
-    <>
-    <h1>hello</h1>
-    </>
+    <div className='App'>
+    <Home/>
+    </div>
   )
 }
 
