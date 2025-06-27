f5dca5b4a63d594689fd421e7938430c38289c99
abz <teshalea000@gmail.com>
2025-06-27 06:44:22 +0300

    deploying the netflix to netlf commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === "true" ? "/Netflix-clone-project/" : "/",
+  base: "/Netflix-clone-project/", // 💥 THIS MUST MATCH your repo name exactly
   plugins: [react()],
 });
