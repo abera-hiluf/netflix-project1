26a941f913b22c1b04d37d0c8110cfe5d22e10a4
abz <teshalea000@gmail.com>
2025-06-27 07:22:40 +0300

    Remove base config for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Netflix-clone-project/", // 💥 THIS MUST MATCH your repo name exactly
   plugins: [react()],
 });
